043aaa7ad9a16ddafab3f79995fbe100cc036027
Vijay Ramakrishnan <vijay120@gmail.com>
2014-07-02 15:11:06 -0700

    general workflow for the server is done

--- boxdrop.js
+++ boxdrop.js
@@ -1,15 +1,44 @@
 var request = require('request');
 var fs = require('fs');
 var path = require('path');
+var http = require('http');
 
-var testLink = "http://web.stanford.edu/group/scspi/_media/working_papers/pfeffer-danziger-schoeni_wealth-levels.pdf"
-var file = fs.createWriteStream("./test1.pdf");
-request.get(testLink).pipe(file);
+var urlRegEx = /([^.\/~%]+.pdf)/;
 
-var r = request.post("https://upload.box.com/api/2.0/files/content", function(error, resp, body) {
-																console.log(resp);
-																//console.log(error);
-															}).auth(null, null, true, "wl4CMAv065ZVO1RYs0kbVepc021dASfH");
-var form = r.form();
-form.append("filename", fs.createReadStream(path.join(__dirname, 'test1.pdf')));
-form.append("folder_id", "0");
+var downloadUrl = function(url, cb) {
+	var sanitizedUrl = urlRegEx.exec(url)[0];
+	var filePath = "/tmp/"+sanitizedUrl;
+	var file = fs.createWriteStream(filePath);
+	request.get(url).pipe(file);
+
+	file.on('finish', function() {
+		cb(filePath);
+	});
+}
+
+var uploadFile = function(filePath) {
+	var r = request.post("https://upload.box.com/api/2.0/files/content", function(error, resp, body) {
+		console.log(resp);
+	}).auth(null, null, true, "LHTBMJ3wHoLzmFcmQTVROszrjgDxn1Iw");
+
+	var form = r.form();
+	form.append("filename", fs.createReadStream(filePath));
+	form.append("folder_id", "0");
+}
+
+
+http.createServer(function(req, res) {
+	if (req.method == 'POST') {
+		var body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+
+        req.on('end', function () {
+        	var keyVal = JSON.parse(body);
+        	var url = keyVal["url"];
+        	downloadUrl(url, uploadFile);
+        });
+	}
+}).listen(8080);
+
